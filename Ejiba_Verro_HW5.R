315a2b9441d9bee25d390373b71fefb4f6d0c197
Verro <vme8@georgetown.edu>
2016-10-13 14:48:19 -0400

    Homework 5
    complete number 5 and 6, working on 7 and 8

--- Ejiba_Verro_HW5.R
+++ Ejiba_Verro_HW5.R
@@ -1,34 +1,45 @@
 #Verro Ejiba
 #Homework 5
+#First download package that has diamond data frame
+install.packages("ggplot2")
+library("ggplot2")
 #1 Print to the console all methods and attributes associated with a dataframe
-data("diamonds")
-attributes(diamonds)
-#methods(diamonds)
-str(diamonds) 
-#check methods again
+df = data.frame(diamonds)
+attributes(df) #Gets the attributes of the dataframe
+str(df) #structure of data frame
 #Determine the number of columns in a dataframe
-length(diamonds)
-summary(diamonds)
-#diamonds[1:20,]
+ncol(df)
+#summary(diamonds)
 #2 Determine how many rows are in a dataframe
-length(row(diamonds))
+nrow(df)
 #3 
 #Extract the column names from a dataframe
-names <- colnames(diamonds)
+colnames(df)
 #print the names of the column names (one per line) to the console
-for (i in 1:length(names)){
-  print(names[i])
+for (i in 1:ncol(df)){
+  print(colnames(df[i]))
 }
 #4 Determine the type of each column (numeric, factor, logical, etc)
-coltypes <- c(sapply(diamonds, class))
-#colTypes <- c(typeof(diamonds))
+coltypes <- c(sapply(df, class))
+coltypes
 #5 Code that will loop through any dataframe and calculate the mean of every numeric column
-for (i in diamonds){
-  if ( is.numeric(i) == T){
-    print(mean(i))
+for (i in (1:ncol(df))){
+  m <- df[,i]
+  if ( is.numeric(m) == T){
+    print(colnames(df)[i])#Label the output with the name of the column
+    print(mean(m)) 
   }
 }
-#print(coltypes[i])
+#6 Loop through any dataframe and create frequency table for every factor column.
+for (i in (1:ncol(df))){
+  f <- df[, i] #extract the column of diamonds
+  if (is.factor(f) == T){
+    print(colnames(df)[i]) #Label the output with the name of the column
+    print(table(f)) #print the frequency
+  }
+}
 
-
-#Label the output with the name of the column
+#7
+for (j in my){
+  print(which(is.na(j)))
+}
