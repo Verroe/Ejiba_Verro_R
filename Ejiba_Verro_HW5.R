8f1113c134793af3d5cb5bb494a80d4ed22a8f30
Verro <vme8@georgetown.edu>
2016-10-24 12:25:14 -0400

    finale with 7 and 8 revised

--- Ejiba_Verro_HW5.R
+++ Ejiba_Verro_HW5.R
@@ -37,6 +37,7 @@ print(lapply(df[, sapply(df, is.factor)], table))
 print(sum(apply(is.na(df), 1, any)))
 #Get the percentage of rows containing NA in any columns by the dividing the sum above by the total number of rows then multiply the result by 100
 100*(sum(apply(is.na(df), 1, any))/nrow(df))
+
 #8 
 f <- function(dataframe){
   #This function accept any dataframe as a parameter and returns a dataframe that contains each pair of column names in the first column in a single string separated by a -, 
@@ -44,39 +45,23 @@ f <- function(dataframe){
   #Parameters: A dataframe
   #Returns: A dataframe with pair of column names and its correlation using pearson method
   
-  name_pairs <- cbind() 
-  correlations <- cbind()
   #remove NA in the data
   ndata <- na.omit(dataframe)
-  mtx <- ndata[sapply(ndata, is.numeric)], method = "pearson"
-  # for (i in (1:ncol(dataframe))){
-  #   name1 = names(diamonds)[i]  #getting the first column name
-  #   
-  #   for (j in (i+1:ncol(dataframe))){  #only getting the columns after the first one
-  #     if  (is.na(names(diamonds)[j]) == FALSE){
-  #       if (names(diamonds)[j] != names(diamonds)[i]){
-  #         name2 =names(diamonds)[j]}   #getting the second column name
-  #       name_pairs <- rbind(name_pairs,paste(name1,name2,sep = "-", collapse = NULL))  #paste them in the "name1-name2" format
-  #       
-  #       if (is.numeric(dataframe[,name1]) == TRUE){  #only calculates the correlation for numeric columns
-  #         if (is.numeric(dataframe[,name2]) == TRUE){
-  #           correlations<- rbind(correlations, cor(dataframe[,name1],dataframe[,name2], method="pearson"))}#calculating the pearson correlations
-  #         else{
-  #           correlations<- rbind(correlations, NA)  #for non-numeric columns, the correletion is NA to avoid errors
-  #         }}
-  #       else{
-  #         correlations<- rbind(correlations, NA)
-  #       }
-  #       
-  #     }
-  #   }
-  #   
-  # }
-  # 
-  # output = data.frame(name_pairs,correlations) #combining two columns
-  return(output)
+  #Creates a correlation matrix
+  corr_mtx <- cor(ndata[sapply(ndata, is.numeric)], method = "pearson")
+  #Prepare to remove duplicates by ignoring the lower triangle part of the matrix
+  corr_mtx[lower.tri(corr_mtx, diag = TRUE)] = NA
+  #creates a dataframe of 3 column table 
+  corr_mtx <- as.data.frame(as.table(corr_mtx))
+  #Remove the duplicates 
+  corr_mtx <- na.omit(corr_mtx)
+  #Write the result as with each pair of the column name separated by "-"
+  #corr_mtx <- corr_mtx[paste(corr_mtx$Var1, corr_mtx$Var2, sep = "-"), ]
+  colnames(corr_mtx) <- c("Var1","Var2","Cor Coef")
+  row.names(corr_mtx) <- c(1:nrow(corr_mtx))
+  
+  return(corr_mtx)
 }
 
-
-print(f(diamonds))
+f(diamonds)
 
